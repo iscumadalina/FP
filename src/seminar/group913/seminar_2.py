d7cd337bb83405842752bbb12bc8e8a034b0875b
Arthur Molnar <arthur486@gmail.com>
2024-10-10 12:20:07 +0300

    First commit seminar 2 / 913

---
+++ src/seminar/group913/seminar_2.py
@@ -0,0 +1,17 @@
+"""
+What do we practice today?
+    - Write a menu-based program
+    - Practice Python list, dict types
+    - How to represent program entities? -- list. vs dict
+    - How to structure the program
+
+Problem statement
+    -- Manage a list of cities. Each city has a name, population and a county
+Program requirements
+    -- Sort them by any of their features (name, pop or county)
+    -- Add a city from the console
+    -- Generate some random cities :)
+    -- Search for a city, using case-insensitive partial string matching
+    -- Exit the program
+"""
+print("Hello Cluj-Napoca!")
