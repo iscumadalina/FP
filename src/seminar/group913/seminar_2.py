6a5aeaacfe2b6f823749d877c602e786a28b12ce
Arthur Molnar <arthur486@gmail.com>
2024-10-10 12:53:18 +0300

    Start the program!

--- src/seminar/group913/seminar_2.py
+++ src/seminar/group913/seminar_2.py
@@ -14,4 +14,72 @@ Program requirements
     -- Search for a city, using case-insensitive partial string matching
     -- Exit the program
 """
-print("Hello Cluj-Napoca!")
+
+
+# --- Functions that deal with how a city is represented
+
+# A city represented using a Python list
+# my_city = ["Constanta", 200_000, "Constanta"]
+
+# A dict is a set of key-to-value mappings; keys must be unique, values not necesarely
+# A city represented using a Python dict
+# my_city = {"name": "Braila", "population": 150_000, "county": "Braila"}
+
+
+def create_city(name: str, population: int, county: str):
+    # list representation
+    # return [name, population, county]
+    # dict representation
+    return {"name": name, "population": population, "county": county}
+
+
+def get_name(city) -> str:
+    # list representation
+    # return city[0]
+    # dict representation
+    return city["name"]
+
+
+def get_population(city) -> int:
+    # return city[1]
+    return city["population"]
+
+
+def get_county(city) -> str:
+    # return city[2]
+    return city["county"]
+
+
+def to_str(city) -> str:
+    return get_name(city) + " with population of " + str(get_population(city)) + " is in " + get_county(city)
+
+
+# --- Functions that implement program requirements
+
+
+# --- User interface functions
+# NOTE The only place where we are allowed to write print(), input() statements
+
+# my_city = create_city("Braila", 150_000, "Braila")
+# print(get_name(my_city), get_population(my_city))
+# print(to_str(my_city))
+
+def start():
+    while True:
+        print("1. Show all cities")
+        print("0. Exit")
+
+        command = input(">")
+
+        if command == 1:
+            pass
+        elif command == 0:
+            # return
+            # exit(0)
+            break
+        else:
+            print("Bad command or file name")
+
+
+# print(locals())
+start()
