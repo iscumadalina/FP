1a2c09193a008480ddddbadc44e8548298b5ea7b
Arthur Molnar <arthur486@gmail.com>
2024-10-15 17:32:55 +0300

    Bugfix and comment in recursive linear search algorithm

--- src/lecture/examples/ex08_sequential_search.py
+++ src/lecture/examples/ex08_sequential_search.py
@@ -6,8 +6,14 @@ def search_iter(data: list, key):
 
 
 def search_rec(data: list, key, pos: int = 0):
+    if len(data) > 997:
+        # The initial execution of the current module and
+        # initial method call are already on the stack, so we have fewer than
+        # 1000 recursive calls available in the default Python implementation ;)
+        raise ValueError("Will overflow the Python stack")
+
     if 0 > pos or pos >= len(data):
         return -1
     if data[pos] == key:
         return key
-    return search_rec(key, data, pos + 1)
+    return search_rec(data, key, pos + 1)
