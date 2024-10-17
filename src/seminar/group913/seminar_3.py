ddf9f41d0250f062ff3241a66b98a5690fd96b88
Arthur Molnar <arthur486@gmail.com>
2024-10-17 15:57:15 +0300

    Finalized seminar code with groups 913, 914.

--- src/seminar/group913/seminar_3.py
+++ src/seminar/group913/seminar_3.py
@@ -30,40 +30,64 @@ from random import random
 """
 
 
-def f_1(n: int):
-    for i in range(10):
-        for j in range(n):
-            print("Hello World")
+# T(n) = 10 * n * 1 => Theta(n), O(n)
+def f_1(n: int):  # input size is given by the value of input parameter n
+    for i in range(10):  # i is between 0 and 9 => 10 times => O(n) due to the inner loop
+        for j in range(n):  # j is between 0 and n - 1 => n times => O(n)
+            print("Hello World")  # O(1)
 
 
+# T(n) = 10 * n * 1 => Theta(n)
+# the first for loop runs for 10 iterations regardless of n's value
 def f_2(n: int):
-    for i in range(n, n + 10):
-        for j in range(n):
+    for i in range(n, n + 10):  # n, ..., n + 9 => O(1)
+        for j in range(n):  # O(n)
             print("Hello World")
 
 
+# T(n) = (n-1) + (n-2) + ... + 1 = n(n-1)/2 => Theta(n^2)
 def f_3(n: int):
-    for i in range(1, n):
-        for j in range(i, n):
+    for i in range(1, n):  # n steps, so O(n)
+        for j in range(i, n):  # (1,2, ..., n-1), (2,...,n-1), ..., (n-1) - number of iterations
             print("Hello World")
 
 
+# T(n) = n * (2n-1) => 2n^2 - n => Theta(n^2)
 def f_4(n: int):
-    for i in range(n):
-        for j in range(2 * i + 1):
+    for i in range(n):  # n steps, so O(n)
+        for j in range(2 * i + 1):  # (0), (0,1,2), (0,1,2,3,4,5,6), ..., (0,...,2n-1)
+            # approximate we go to 2n-1 each time => larger number of steps
             print("Hello World")
 
 
+counter = 0
+
+
+# T(n) = n^2 * n^2 => Theta(n^4)
 def f_5(n: int):
-    for i in range(n ** 2):
-        for j in range(i):
-            print("Hello World")
+    global counter  # consider the module-level variable counter
+    for i in range(n ** 2):  # n^2 (starts at 0, ends at n^2 - 1)
+        for j in range(i):  # n^2
+            # print("Hello World")  # O(1)
+            counter += 1
 
 
+# for index in range(1, 31):
+#     f_5(index)
+#     print(index, counter)
+#     counter = 0
+
+# sum = 0
+# for i in list(range(1_000_000_000)):
+#     sum += i
+#
+# print(sum)
+
+# T(n) = n * log_2(n) => Theta(n * log_2(n))
 def f_6(n: int):
-    for i in range(n):  # n
+    for i in range(n):  # n steps
         t = 1
-        while t < n:
+        while t < n:  # runs log_2(n) times
             print("Hello World")
             t *= 2
 
@@ -73,6 +97,7 @@ def f_6(n: int):
 """
 
 
+# T(n, m) = n + m => Theta(n + m)
 def f_7(n, m: int):
     for i in range(0, n):  # n loops
         print("Hello World")
@@ -80,6 +105,7 @@ def f_7(n, m: int):
         print("Hello World")
 
 
+# T(n) = 2 * n => Theta(n)
 def f_8(n, m: int):
     for i in range(0, n):
         print("Hello World")
@@ -87,6 +113,7 @@ def f_8(n, m: int):
         print("Hello World")
 
 
+# T(n) = 2*n * n => Theta(n^2)
 def f_9(n: int):
     for i in range(n):
         for j in range(n):
@@ -95,9 +122,10 @@ def f_9(n: int):
             print("Hello World")
 
 
+# T(n) = n^2
 def f_10(n: int):
     for i in range(n):
-        for j in range(n, i, -1):
+        for j in range(n, i, -1):  # between n and i, decreasing by 1
             print("Hello World")
 
 
@@ -122,16 +150,63 @@ def f_12(n: int) -> None:
     Analyze the time and space complexity 
 """
 
+"""
+    Time complexity
+        n = len(data)
+        Theta(n * log_3(n))
+    
+    Extra-space complexity
+        data = initial list, we do not count it
+        O(1) because we don't allocate memory space based on the size of the initial input
+"""
+
 
 def f_13(data: list) -> int:
     data_sum = 0
-    for el in data:
-        j = len(data)
-        while j > 1:
+    for el in data:  # runs n times
+        j = len(data)  # j = n
+        while j > 1:  # log_3(n)
             data_sum += el * j
             j = j // 3
     return data_sum
 
+
+"""
+    Time complexity:
+        n = len(data)
+
+    T(n) = 1, n < 2
+    T(n) = 2 * T(n/2) + 1, n >= 2
+    
+    T(n) = 2 * T(n/2) + 1
+    T(n/2) = 2 * T(n/4) + 1
+    T(n/4) = 2 * T(n/8) + 1
+    T(n/8) = 2 * T(n/16) + 1
+    
+    T(n) = 2 * [2 * T(n/4) + 1] + 1
+    T(n) = 4 * T(n/4) + 2 + 1
+    T(n) = 4 * [2 * T(n/8) + 1] + 2 + 1
+    T(n) = 8 * T(n/8) + 4 + 2 + 1
+    
+    T(n) = 16 * T(n/16) + 8 + 4 + 2 + 1
+    at one point we get lists of length 1
+    at that point we divided the list k times, so 2^k = n 
+    so ...
+    T(n) = 2^k * T(1) + 2^(k-1) + 2^(k-2) + ... + 2^0 # remember that T(1) = 1, base case of the recursion :)
+    T(n) = (2^(k+1) - 1) / (2 - 1) # known formula
+    T(n) = 2n - 1 => Theta(n)
+    
+    # assuming that data[:m] is an O(1) operation!
+    
+    Space complexity!
+        n = len(data)
+        
+    T(n) = 1, n < 2
+    T(n) = 2 * T(n/2) + n, n >= 2
+"""
+
+
+# TODO Make the calculation for space complexity, like in the case of time complexity
 
 def f_14(data: list) -> int:
     if len(data) == 0:
@@ -139,7 +214,7 @@ def f_14(data: list) -> int:
     if len(data) == 1:
         return data[0]
     m = len(data) // 2
-    return f_14(data[:m]) + f_14(data[m:])
+    return f_14(data[:m]) + f_14(data[m:])  # if n = len(data), then n/2 = len(data[:m])
 
 
 def f_15(n: int) -> int:
@@ -175,11 +250,11 @@ def f_17(data: list) -> int:
     j = 0
     m = 0
     c = 0
-    while i < len(data):
+    while i < len(data):  # n steps
         if data[i] == data[j]:
             c += 1
         j += 1
-        if j >= len(data):
+        if j >= len(data):  # n - 1, n - 2, ..., 1 steps
             if c > m:
                 m = c
             c = 0
@@ -187,6 +262,30 @@ def f_17(data: list) -> int:
             j = i
     return m
 
+
+"""
+1. Determines the frequency of the element that appears most often in the list
+2. Time complexity is Theta(n^2)
+"""
+
+
+def f_17_better(data: list) -> int:
+    map = {}
+    for e in data:  # O(n)
+        if e in map:
+            map[e] += 1
+        else:
+            map[e] = 1
+
+    max_freq = 0
+    for key in map:  # O(n)
+        max_freq = max(max_freq, map[key])
+        # if map[key] > max_freq:
+        #     max_freq = map[key]
+    return max_freq
+
+
+print(f_17([1, 2, 2, 2, 3, 3, 1]))
 
 """
     What is the time complexity when implementing the following algorithm. How can this be 
--- src/seminar/group914/seminar_3.py
+++ src/seminar/group914/seminar_3.py
@@ -24,42 +24,60 @@
 from random import random
 
 
+# T(n) = 10 * n * 1 => Theta(n)
 def f_1(n: int):
-    for i in range(10):
-        for j in range(n):
-            print("Hello World")
+    for i in range(10):  # O(1)
+        for j in range(n):  # liniar time O(n)
+            print("Hello World")  # constant time O(1)
 
 
+# T(n) = 10 * n * 1 => Theta(n)
 def f_2(n: int):
-    for i in range(n, n + 10):
-        for j in range(n):
+    for i in range(n, n + 10):  # n, n + 1, ..., n + 9 (constant time) => O(1)
+        for j in range(n):  # 0, ..., n
             print("Hello World")
 
 
+# T(n) = (1, ..., n - 1) + (2, ..., n - 1) + ... + (n - 1) => (n-1)(n-2)/2 => O(n^2)
+#        (values of j)
 def f_3(n: int):
-    for i in range(1, n):
+    for i in range(1, n):  # O(n)
         for j in range(i, n):
             print("Hello World")
 
 
+# T(n) = 2*n^2 => O(n^2)
 def f_4(n: int):
-    for i in range(n):
-        for j in range(2 * i + 1):
+    for i in range(n):  # O(n)
+        for j in range(2 * i + 1):  # max value is (2n  - 1) => O(n)
             print("Hello World")
+
+
+# T(n) = n^4 => O(n^4)
+count = 0
 
 
 def f_5(n: int):
-    for i in range(n ** 2):
-        for j in range(i):
-            print("Hello World")
+    global count  # consider the global count variable
+    for i in range(n ** 2):  # O(n^2)
+        for j in range(i):  # O(n^2)
+            # print("Hello World")
+            count += 1
 
 
+# empirical evaluation :)
+# for i in range(1, 31):
+#     count = 0
+#     f_5(i)
+#     print(i, count)
+
+# T(n) = n * log_2(n) so the algorithm is O(n*log_2(n))
 def f_6(n: int):
     for i in range(n):  # n
         t = 1
-        while t < n:
+        while t < n:  # we approximate log_2(n) each time, even if that's only the last run
             print("Hello World")
-            t *= 2
+            t *= 2  # we can multiply "t" a number of log_2(n) times before it's greater than "n"
 
 
 """
@@ -67,6 +85,7 @@ def f_6(n: int):
 """
 
 
+# T(n, m) = n + m => O(n+m)
 def f_7(n, m: int):
     for i in range(0, n):  # n loops
         print("Hello World")
@@ -74,6 +93,7 @@ def f_7(n, m: int):
         print("Hello World")
 
 
+# T(n, m) = 2n => O(n)
 def f_8(n, m: int):
     for i in range(0, n):
         print("Hello World")
@@ -81,6 +101,7 @@ def f_8(n, m: int):
         print("Hello World")
 
 
+# T(n) = 2 * n^2 => O(n^2)
 def f_9(n: int):
     for i in range(n):
         for j in range(n):
@@ -89,26 +110,30 @@ def f_9(n: int):
             print("Hello World")
 
 
+# T(n) = n^2 => Theta(n^2)
 def f_10(n: int):
-    for i in range(n):
-        for j in range(n, i, -1):
+    for i in range(n):  # n
+        for j in range(n, i, -1):  # n -> i + 1 => n / 2
             print("Hello World")
 
 
+# T(n, m) = n + m => Theta(n+m)
 def f_11(n: int, m: int) -> None:
     a = 0
     b = 0
     for i in range(n):
-        a = a + random()
+        a = a + random()  # what's the time complexity of random()?, we assume that its O(1)
 
     for i in range(m):
         b = b + random()
 
 
+# T(n) = n/2 * log_2(n) => Theta(n * log_2(n))
 def f_12(n: int) -> None:
     k = 0
-    for i in range(n // 2, n):
-        for j in range(2, n, pow(2, j)):
+    for i in range(n // 2, n):  # // is integer division => n/2 times
+        j = 2
+        for j in range(2, n, pow(2, j)):  # log_2(n)
             k = k + n / 2
 
 
@@ -116,15 +141,55 @@ def f_12(n: int) -> None:
     Analyze the time and space complexity 
 """
 
+"""
+Time complexity
+    n = len(data)
+    T(n) = n * log_3(n) => Theta(n * log_3(n))
+    
+Space complexity
+    T(n) = 1 => Theta(1) # the allocated memory does not depend on the length of the list
+"""
+
 
 def f_13(data: list) -> int:
     data_sum = 0
-    for el in data:
-        j = len(data)
-        while j > 1:
+    for el in data:  # O(n)
+        j = len(data)  # j = n
+        while j > 1:  # O(log_3(n))
             data_sum += el * j
             j = j // 3
     return data_sum
+
+
+"""
+Time complexity
+    n = len(data)
+    
+    T(n) = 1, if n < 2
+    T(n) = 2 * T(n/2) + 1, n >= 2
+    
+    T(n) = 2 * T(n/2) + 1
+    T(n/2) = 2 * T(n/4) + 1
+    T(n/4) = 2 * T(n/8) + 1
+    
+    T(n) = 2 * [2 * T(n/4) + 1] + 1
+    T(n) = 4 * T(n/4) + 2 + 1
+    T(n) = 4 * [2 * T(n/8) + 1] + 2 + 1
+    T(n) = 8 * T(n/8) + 4 + 2 + 1
+    
+    T(n) = 16 * T(n/16) + 8 + 4 + 2 + 1
+    there exists a value k so that 2^k = n (or k = log_2(n) )
+    what do we get after "k" iterations?
+    
+    T(n) = 2^k * 1 + 2^(k-1) + ... + 2^0 = 2^(k+1) => O(n)
+    
+Space complexity
+    T(n) = 2 * T(n/2) + n (n is from the 2 slices of the original list) 
+    ...
+"""
+
+
+# TODO Continue the calculation for space complexity
 
 
 def f_14(data: list) -> int:
@@ -133,6 +198,13 @@ def f_14(data: list) -> int:
     if len(data) == 1:
         return data[0]
     m = len(data) // 2
+    """
+    What happens on the follosing line? 
+    1. Slices of the data list are created (data[:m]) -> part of the list is copied
+    2. f_14 is called (twice, in succession)
+    3. The + operation is carried out
+    4. Function returns
+    """
     return f_14(data[:m]) + f_14(data[m:])
 
 
