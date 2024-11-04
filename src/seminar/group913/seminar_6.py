b20348e76697602da1312be7d3013c5e9f12330c
Arthur Molnar <arthur486@gmail.com>
2024-11-04 12:20:14 +0200

    Starter code for seminar 6 (groups 914, 913).

---
+++ src/seminar/group913/seminar_6.py
@@ -0,0 +1,45 @@
+"""
+Write an application that manages a number of cities
+Each city has a unique name (string), a county (string) and a population (integer).
+The application will have a menu-driven user interface and will provide the following features:
+
+    1. Add a city
+        - adds the city with the given id, county and population to the list.
+        - error if giving the existing name, the name, county are empty or population is negative
+
+    2. Delete a city
+        - deletes the city with the given name from the list
+        - error if non-existing name is provided
+
+    3. Show all cities
+        - shows all cities in descending order of their population
+
+    4. Show cities whose population is > than given one, ordered in descending order of population
+
+    5. exit
+        - exit the program
+
+    Observations:
+        - Add 10 random cities at program startup
+        - Create a list-based and a dict-based representation of the City entity
+        - Write specifications for non-UI functions
+        - Write tests for the non-trivial, non-UI functions
+        - Use TypeError and ValueError to handle input and program errors
+        - Each function does one thing only, and communicates via parameters and return value
+        - The program reports errors to the user. It must also report errors from non-UI functions!
+        - Make sure you understand the city's representation
+        - Try to reuse functions across functionalities (Less code to write and test)
+        - Don't use global variables!
+"""
+
+"""
+    City representation functions are below 
+"""
+
+"""
+    Program functionalities (non-UI) are below
+"""
+
+"""
+    UI code is below
+"""
---
+++ src/seminar/group914/seminar_6.py
@@ -0,0 +1,46 @@
+"""
+Write an application that manages a group of students.
+Each student has a unique id (string), a name (string) and a grade (integer).
+The application will have a menu-driven user interface and will provide the following features:
+
+    1. Add a student
+        - adds the student with the given id, name and grade to the list.
+        - error if giving existing id, the name or grade fields not given or
+        empty
+
+    2. Delete a student
+        - deletes the student with the given id from the list
+        - error if non-existing id given
+
+    3. Show all students
+        - shows all students in descending order of their grade
+
+    4. Show students whose grade is > than given one, ordered in descending order of grade
+
+    5. exit
+        - exit the program
+
+    Observations:
+        - Add 10 random students at program startup
+        - Create a list-based and a dict-based representation of the Student entity
+        - Write specifications for non-UI functions
+        - Write tests for the non-trivial, non-UI functions
+        - Use TypeError and ValueError to handle input and program errors
+        - Each function does one thing only, and communicates via parameters and return value
+        - The program reports errors to the user. It must also report errors from non-UI functions!
+        - Make sure you understand the student's representation
+        - Try to reuse functions across functionalities (Less code to write and test)
+        - Don't use global variables!
+"""
+
+"""
+    Student representation functions are below 
+"""
+
+"""
+    Program functionalities (non-UI) are below
+"""
+
+"""
+    UI code is below
+"""
